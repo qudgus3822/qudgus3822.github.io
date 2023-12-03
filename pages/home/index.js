d2232b3a1efd0c471cd6b97bfe453207bdd4d77d
qudgus3822 <xgame1123@naver.com>
2023-12-03 19:16:26 +0900

    .

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,7 +1,5 @@
 import React from "react";
-import Link from "next/link";
 import { useEffect, useState } from "react";
-import { readFile, uploadFile } from "../db/fileProcess";
 
 export default function IndexPage() {
     let hosturl = "";
