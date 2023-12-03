5b197b688964f6c9871dae50e26a59de5ebed148
kim byeong hyun <xgame1123@naver.com>
2023-12-03 17:40:11 +0900

    .

--- views/common/organisms/BlogContentList.js
+++ views/common/organisms/BlogContentList.js
@@ -1,10 +1,32 @@
 import { useEffect, useState } from "react"
+import { readFile } from "../../../db/fileProcess";
 
 export default function BlogContentList() {
 
     const [data, setData] = useState([]);
+    const [bindData, setBindData] = useState([]);
     useEffect(() => {
+        readFile("blog.json").then((res) => {
+            if (res && res.length > 0) {
+                let tempData = [];
+                let resultData = [];
+                let index = 0;
+                for (let item of res) {
 
+                    tempData.push(item[index]);
+                    index++;
+
+                    if (tempData.length == 3) {
+                        resultData.push(tempData);
+                        tempData = [];
+                    }
+
+                    if (index == res.length && tempData.length > 0) {
+                        resultData.push(tempData);
+                    }
+                }
+            }
+        })
     }, []);
 
     return (<>
