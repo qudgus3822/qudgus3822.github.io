05302249b122b0c59abf26f289736f3b34d77236
qudgus3822 <xgame1123@naver.com>
2024-01-09 18:48:51 +0900

    .

--- components/speedClick/Cell.js
+++ components/speedClick/Cell.js
@@ -1,1 +1,23 @@
+import React from "react";
 
+function Cell({ num, cellClickHandler }) {
+
+    const clickHandler = () => {
+        cellClickHandler(num);
+    }
+
+    return (<>
+        <div style={Container} onClick={clickHandler}>{num}</div>
+        {/* <button>test</button> */}
+    </>);
+}
+
+const Container = {
+    border: "1px solid red",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    fontSize: "30px"
+}
+
+export default Cell;
